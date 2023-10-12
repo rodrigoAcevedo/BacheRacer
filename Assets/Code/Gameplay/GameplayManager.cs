24c8a162f66a23b3d2bb9e42ddfe43de82342e3c
Rodrigo Acevedo <rodriacvdo@gmail.com>
2023-10-12 00:46:05 -0300

    Dynamic instantiation of road and player (first attempt)

--- Assets/Code/Gameplay/GameplayManager.cs
+++ Assets/Code/Gameplay/GameplayManager.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    [SerializeField] private GameObject RoadPrefab;
+    [SerializeField] private GameObject Player;
+
+    [SerializeField] private Transform playerStart;
     // Start is called before the first frame update
     void Start()
     {
+        Transform parent = transform.parent;
         
+        GameObject playerInstance = Instantiate(Player, parent);
+        playerInstance.transform.position = playerStart.position;
+        
+        Vector3 firstRoadPos = new Vector3(0, 0, 1);
+        Instantiate(RoadPrefab, firstRoadPos, Quaternion.identity, parent);
     }
 
     // Update is called once per frame
